c8f2b9834c002e2db0a4971954067d395e98f536
#FAUZAN NADHIF MUHAMMAD# <FAUZANNA001@student.main.ntu.edu.sg>
2017-11-09 13:10:20 +0800

    Add MovieList class

--- src/Movie.java
+++ src/Movie.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 
 public class Movie {
+
 	private String Title;
 	private String Status;
 	private String Synopsis;
@@ -19,4 +20,81 @@ public class Movie {
 		//TODO: FileIO
 	}
 	
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+	public String getSynopsis() {
+		return Synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		Synopsis = synopsis;
+	}
+
+	public String getDirector() {
+		return Director;
+	}
+
+	public void setDirector(String director) {
+		Director = director;
+	}
+
+	public String getType() {
+		return Type;
+	}
+
+	public void setType(String type) {
+		Type = type;
+	}
+
+	public ArrayList<String> getCast() {
+		return Cast;
+	}
+
+	public void setCast(ArrayList<String> cast) {
+		Cast = cast;
+	}
+
+	public Float getOverallRating() {
+		return OverallRating;
+	}
+
+	public ArrayList<Integer> getRating() {
+		return Rating;
+	}
+
+	public void addRating(Integer newRating) {
+		this.Rating.add(newRating);
+		this.OverallRating = (OverallRating*(Rating.size()-1)+newRating)/Rating.size();
+	}
+
+	public ArrayList<String> getReview() {
+		return Review;
+	}
+
+	public void addReview(String review) {
+		this.Review.add(review);
+	}
+
+	public String getAgeRating() {
+		return AgeRating;
+	}
+
+	public void setAgeRating(String ageRating) {
+		AgeRating = ageRating;
+	}
+
 }
---
+++ src/MovieList.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class MovieList {
+	private ArrayList<Movie> Movies;
+	MovieList(){
+		//FILEIO
+	}
+	public void addMovie(Movie newMovie){
+		Movies.add(newMovie);
+		// FILEIO
+	}
+	
+	public ArrayList<Movie> getMovie(){
+		return this.Movies;
+	}
+	
+	public ArrayList<Movie> sortMovie(int choice){
+		ArrayList<Movie> sortedMovies = Movies;
+		if (choice == 1){
+			System.out.println("Sorting by ticket sales");
+		}
+		else if (choice == 2){
+			System.out.println("Sorting by overall reviewers ratings");
+		}
+		else {
+			System.out.println("Wrong Choice!");
+		}
+		return sortedMovies;
+	}
+}
