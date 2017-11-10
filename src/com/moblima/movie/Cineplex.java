8f36c763037a070420c8124ffc215a6655758818
unknown <eugehansyafesi@yahoo.co.id>
2017-11-09 19:16:24 -0800

    Fix error

--- src/com/moblima/movie/Cineplex.java
+++ src/com/moblima/movie/Cineplex.java
@@ -45,7 +45,7 @@ public class Cineplex{
         try{
             this.Location = readLocation("Database");            
         }catch (IOException e){
-            System.out.println("IOException > " + e.getMessage())
+            System.out.println("IOException > " + e.getMessage());
         }
     }
 
