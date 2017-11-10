c2e480fc12867522aaf5aa956c9ff9f7e4eb42ae
pocoyopin <32367224+pocoyopin@users.noreply.github.com>
2017-11-10 10:35:21 +0800

    Edited the code

--- src/com/moblima/Application/UserApplication.java
+++ src/com/moblima/Application/UserApplication.java
@@ -37,31 +37,47 @@ public class UserApplication {
 				break;
 				
 			case 2: //list movie
+				MovieList.getMovie()
 				break;
 				
 			case 3: //select movie, list the details
+				System.out.println("Here are the movies available: ");
+				MovieList.getMovie();
+				System.out.println("Please choose the movie you want to view");
+				int movieInput = sc.next();
+					
+				System.out.println("Here is the details of the movie: ");
+				System.out.print("Title: ");
+				Movie.getTitle();
+				System.out.print("Showing status: ");
+				Movie.getStatus();
+				System.out.print("Synopsis: ");
+				Movie.getSynopsis();
+				System.out.print("Director: ");
+				Movie.getDirector();
+				System.out.print("Casts: ");
+				Movie.getCast();
+				System.out.print("Overall reviewer rating: ");
+				Movie.getOverallRating();
+				System.out.print("Reviews: ");
+				Movie.getReview();
+					
 				break;
 				
 			case 4: //choose cineplex; choose movie; choose time; show: array list of seats
 				break;
 				
-			case 5: //choose cineplex; choose movie; choose time; choose seat; show: tickets, seats, amount to be paid; approve?
+			case 5: //choose cineplex; choose movie; choose time; choose seat; show: tickets, seats, amount to be paid; approve? (if yes, add to booking history)
 				break;
 				
 			case 6: //list all movies booked, show: transaction id, movie, cinema, cineplex, date/time, tickets, seats, total amount
+				User.getBookingHistory();
 				break;
 				
 			case 7: 
 				System.out.println("Sort by ticket sales or ratings? 1 for ticket sales, 2 for ratings");
 				int inputTop5 = sc.nextInt();
-				if (inputTop5!=1 || inputTop5!=2)
-					System.out.println("Not a valid input! Please enter another input: ");
-				else if (inputTop5 == 1){
-					//list the top 5 movie by ticket sales
-				}
-				else if (inputTop5 == 2){
-					//list the top 5 movie by ratings
-				}	
+				MovieList.sortMovie(inputTop5);	
 				break;
 			
 			case 8:
