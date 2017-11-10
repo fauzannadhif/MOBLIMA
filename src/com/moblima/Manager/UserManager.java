99c262817c358ba690e29c182bfec682faf0cfe9
fauzannadhif <fauzannadhifm98@gmail.com>
2017-11-10 11:50:13 +0800

    Update UserManager.java

--- src/com/moblima/Manager/UserManager.java
+++ src/com/moblima/Manager/UserManager.java
@@ -1,5 +1,19 @@
 package com.moblima.Manager;
 
 public class UserManager{
+    private String bookingID;
+    private User user;
+    public UserManager(User user){
+        this.user = user;
+    }
+    
+    public string bookMovie(Movie movie,GregorianCalendar date,Cinema cinema,Cineplex cineplex){
+        this.bookingID = cinema.getCinemaCode+(String)date;
+        user.addBookingHistory(bookingID);
+        TicketPrice ticketprice = new TiketPrice(false,user.getAge(),Movie.getType(),Cinema.getType())
+        Double price = ticketprice.getPrice();
+        System.out.println("Your price = "+price);
+        return bookingID;
+    }
     
 }
