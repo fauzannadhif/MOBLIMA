e327f670df4928e01eeaffe66390ea482127072d
Gehan Syafesi <eugehansyafesi@yahoo.co.id>
2017-11-12 18:36:08 -0800

    finish db implementation in cineplex

--- src/com/moblima/movie/Cineplex.java
+++ src/com/moblima/movie/Cineplex.java
@@ -12,6 +12,27 @@ public class Cineplex{
     private String Location;
     private static final String separator = "|";
 
+    // Parse String Array into CinemaList
+    public ArrayList<Cinema> readCinemaList(String filename) throws IOException{
+        ArrayList<Cinema> resultCinemaList = new ArrayList<Cinema>();
+        ArrayList<String> StringArray = read(filename);
+        Cinema cinema;
+        for (int i=0; i < StringArray.size(); i++){
+            String st = StringArray.get(i);
+            StringTokenizer star = new StringTokenizer(st, separator);
+            String name = star.nextToken().trim();
+            if (name == Name){
+                while (star != null){
+                    cinema = new Cinema(star.nextToken(), Name);
+                    resultCinemaList.add(cinema);
+                }
+            }
+        }
+        return resultCinemaList;
+    }
+
+
+    // Parse String Array into Location
     public String readLocation(String filename) throws IOException{
         ArrayList<String> StringArray = read(filename);
         for (int i=0; i < StringArray.size(); i++){
@@ -23,9 +44,10 @@ public class Cineplex{
                 return location;
             }
         }
-        return "None";
+        return "";
     }
 
+    // Text Parser from the file
     public static ArrayList<String> read(String filename) throws IOException{
         ArrayList<String> data = new ArrayList<String>();
         Scanner sc = new Scanner(new FileInputStream(filename));
@@ -43,7 +65,12 @@ public class Cineplex{
     public Cineplex(String name){
         this.Name = name;
         try{
-            this.Location = readLocation("Database");            
+            this.CinemaList = readCinemaList("Database2.txt");
+        }catch(IOException e){
+            System.out.println("IOException > " + e.getMessage());
+        }
+        try{
+            this.Location = readLocation("Database.txt");            
         }catch (IOException e){
             System.out.println("IOException > " + e.getMessage());
         }
