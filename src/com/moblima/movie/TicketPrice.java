665361a540b49392ac44d85451c8abe073f59209
nadhif <fauzannadhifm98@gmail.com>
2017-11-16 10:56:29 +0800

    Modifiers.txt edited

--- src/com/moblima/movie/TicketPrice.java
+++ src/com/moblima/movie/TicketPrice.java
@@ -15,7 +15,7 @@ public class TicketPrice {
 	private Double[] Modifier;
 	public TicketPrice(){
 		try {
-			File DatabaseFile = new File("../../../../data/ShowTimeList.txt");
+			File DatabaseFile = new File("../../../../data/Modifiers.txt");
             this.Modifier = readModifiers(DatabaseFile);
 		} catch (Exception e) {
 			System.out.println("IOException > " + e.getMessage());
